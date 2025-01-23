56be7be26a0c48e25050f5163adfef3cc4fe3627
한주형 <wngud4950@gmail.com>
2025-01-24 05:47:26 +0900

    feat: 커버 이미지 사이즈 체크 로직 추가

--- src/main/java/nextstep/courses/domain/CoverImage.java
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -1,11 +1,12 @@
 package nextstep.courses.domain;
 
-import java.awt.*;
+import nextstep.qna.domain.Dimension;
 
 public class CoverImage {
     private int size;
     private ImageType imageType;
     private Dimension dimension;
+    private int MAX_SIZE = 1 * 1024 * 1024;
 
     public CoverImage(int size, ImageType imageType, int width, int height) {
         checkSize(size);
@@ -15,5 +16,8 @@ public class CoverImage {
     }
 
     private void checkSize(int size) {
+        if (size > MAX_SIZE) {
+            throw new IllegalArgumentException("이미지 크기는 1MB 이하이어야 합니다.");
+        }
     }
 }
--- src/test/java/nextstep/courses/domain/CoverImageTest.java
+++ src/test/java/nextstep/courses/domain/CoverImageTest.java
@@ -7,19 +7,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CoverImageTest {
 
+    private int DEFAULT_WIDTH = 300;
+    private int DEFAULT_HEIGHT = 200;
     @Test
     void 커버_이미지를_생성한다() {
-        assertThat(new CoverImage(100, ImageType.JPEG, 100, 100)).isNotNull();
+        assertThat(new CoverImage(100, ImageType.JPEG, DEFAULT_WIDTH, DEFAULT_HEIGHT)).isNotNull();
     }
 
     @Test
     void 이미지_1MB_이하여야_한다() {
-        assertThat(new CoverImage(1024 * 1024 - 1, ImageType.JPEG, 100, 100)).isNotNull();
+        assertThat(new CoverImage(1024 * 1024 - 1, ImageType.JPEG, DEFAULT_WIDTH, DEFAULT_HEIGHT)).isNotNull();
     }
 
     @Test
-    void 이미지_1MB_이상이면_예외가_발생한다() {
-        assertThatThrownBy(() -> new CoverImage(1024 * 1024, ImageType.JPEG, 100, 100))
-                .isInstanceOf(IllegalArgumentException.class);
+    void 이미지_1MB_초과면_예외가_발생한다() {
+        assertThatThrownBy(() -> new CoverImage(1024 * 1024 + 1, ImageType.JPEG, DEFAULT_WIDTH, DEFAULT_HEIGHT))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미지 크기는 1MB 이하이어야 합니다.");
     }
 }
